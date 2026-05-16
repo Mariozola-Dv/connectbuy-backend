0abc6f6e2f2f08b56273f31025263b5ada758014
Mario Zola <mariozola69email@gmail.com>
2026-05-16 17:23:02 -0300

    fix cors production

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true, // permite frontend local e produção (Vercel)
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://connectbuy-frontend.vercel.app'
+    ],
     credentials: true,
   });
 
